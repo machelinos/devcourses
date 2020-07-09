45b7733e3b752c5c1d6d37479d28598ae0b346ee
Marcel Aristides Cabrera Gomez <marcel.cabrera@gmail.com>
2020-07-09 17:45:48 -0500

    FEM course : JS the hard parts - Async exercises

---
+++ javascript/frontendmasters/01-jspath/02-js-the-hard-parts/03-async/app.js
@@ -0,0 +1,226 @@
+/* Challenge 1
+Thinking point (no writing code necessary for this challenge): Inspect the code given to you in Challenge 1. In what order should the console logs come out? Howdy first or Partnah first?
+ */
+function sayHowdy() {
+    console.log('Howdy');
+}
+  
+function testMe() {
+    setTimeout(sayHowdy, 0);
+    console.log('Partnah');
+}
+
+/* Challenge 2
+Create a function delayedGreet that console logs welcome after 3 seconds.
+ */
+function delayedGreet(){
+    setTimeout(function(){
+        console.log('Welcome');
+    }, 3000);
+}
+
+/* Challenge 3
+Create a function helloGoodbye that console logs hello right away, and good bye after 2 seconds.
+ */
+function helloGoodbye(){
+    console.log('hello');
+
+    setTimeout(function(){
+        console.log('good bye');
+    }, 2000);
+}
+
+/* Challenge 4
+Create a function brokenRecord that console logs hi again every second. Use the End Code button to stop the console logs when you are satisfied that it is working.
+ */
+function brokenRecord(){
+    console.log('hi again');
+
+    setTimeout(brokenRecord, 1000);
+}
+
+/* 
+Challenge 5
+Create a function limitedRepeat that console logs hi for now every second, but only for 5 seconds. Research how to use clearInterval if you are not sure how to do this. */
+function limitedRepeat(){
+    var counter=0;
+
+    function count(){
+        if(counter>4){
+            clearTimeout(timer);
+        }else{
+            console.log('hi for now');
+        }
+        counter++;
+        var timer = setTimeout(count, 1000);
+    }
+
+    count();
+}
+
+/* Challenge 6
+Write a function called everyXsecsForYsecs that will accept three arguments: a function func, a number interval, and another number duration.
+
+everyXsecsForYsecs will execute the given function every interval number of milliseconds, but then automatically stop after duration milliseconds.
+
+Then pass the below sayHi function into an invocation of everyXsecsForYsecs with 1000 interval time an 5000 duration time.
+What do you expect to happen? */
+function everyXsecsForYsecs(func, interval, duration){
+    let timerFunction = setInterval(func,interval*1000);
+
+    setTimeout(function(){
+        clearInterval(timerFunction);
+    },duration*1000);
+
+}
+
+// Uncomment the following lines to check your work!
+function theEnd() {
+  console.log('This is the end!');
+}
+//everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+/* Challenge 7
+Write a function delayCounter that accepts a number (called 'target') as the first argument and a number of milliseconds (called 'wait') as the second argument, and returns a function.
+
+When the returned function is invoked, it should log to the console all of the numbers between 1 and the target number, spaced apart by 'wait' milliseconds. */
+function delayCounter(target, wait){
+    let number=1;
+
+    function logNumbers(){
+        let logTimer = setInterval(function(){
+            console.log(number);
+            number++;
+        }, wait);
+
+        setTimeout(function(){
+            clearInterval(logTimer);
+        }, target*1000);
+    }
+
+    return logNumbers;
+
+}
+
+const countLogger = delayCounter(3, 1000)
+//countLogger();
+// After 1 second, log 1
+// After 2 seconds, log 2
+// After 3 seconds, log 3
+
+
+
+/* 
+Challenge 8
+Write a function, promised, that takes in a value. This function will return a promise that will resolve after 2 seconds.
+
+Hint: take a look at the Promise object docs on MDN.*/
+
+function promised(value){
+    let promise = {
+        then : function(fn){
+            setTimeout(function(){
+                fn(value);
+            }, 2000);
+        }
+    }
+    return promise;
+}
+
+const createPromise = promised('wait for it...');
+//createPromise.then((val) => console.log(val)); 
+//will log "wait for it..." to the console after 2 seconds
+
+
+
+/* Challenge 9
+Write a SecondClock class, with two methods: start and reset.​
+start: upon invocation, invokes a callback (this.cb, defined in the constructor) on an argument every second, with the argument to the callback being the current seconds "value".
+
+In other words, the callback gets invoked every second on the "seconds hand" of the clock. Always start with 1 and don't utilize the seconds value of the current computer clock time.
+
+The first "tick" with value 1 occurs 1 second after the initial "secondClock" invocation.
+The second "tick" with value 2 occurs 2 seconds after the initial "secondClock" invocation.
+...
+The sixtieth "tick" with value 60 occurs 60 seconds after the initial "secondClock" invocation.
+The sixty-first "tick" with value 1 occurs 61 seconds after the initial "secondClock" invocation.
+The sixty-second "tick" with value 2 occurs 62 seconds after the initial "secondClock" invocation.
+etc.
+reset: upon invocation, completely stops the "clock".
+Also resets the time back to the beginning.
+​
+Hint: look up setInterval and clearInterval. */
+/*Challenge 9
+
+function SecondClock(cb){
+    this.cb = cb;
+    this.timer = undefined;
+}
+
+SecondClock.prototype.start = function(){
+    this.timer = setInterval(this.cb,1000);
+}
+
+SecondClock.prototype.reset = function(){
+    clearInterval(this.timer);
+} */
+
+
+
+//Challenge 9 in ES6 Classes
+class SecondClock{
+    constructor(cb){
+        this.cb = cb;
+        this.timer = undefined;
+    }
+
+    start(){
+        this.timer = setInterval(this.cb, 1000);
+    }
+
+    reset(){
+        clearInterval(this.timer);
+    }
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const clock = new SecondClock((val) => { console.log(val) });
+//console.log("Started Clock.");
+//clock.start('tick');
+/* setTimeout(() => {
+    clock.reset();
+    console.log("Stopped Clock after 6 seconds.");
+}, 6000);
+ */
+/* 
+Challenge 10
+Write a function called debounce that accepts a function and returns a new function that only allows invocation of the given function after interval milliseconds have passed since the last time the returned function ran.
+
+Additional calls to the returned function within the interval time should not be invoked or queued, but the timer should still get reset.
+
+For examples of debouncing, check out https://css-tricks.com/debouncing-throttling-explained-examples/ */
+function debounce(fn, interval){
+
+    let timerStart = new Date();
+
+    function invoke(){
+        let invocationTime = new Date();
+
+        if(invocationTime-timerStart > interval){
+            timerStart = new Date();
+            return fn();
+        }
+
+    }
+
+    return invoke;
+
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+function giveHi() { return 'hi'; }
+const giveHiSometimes = debounce(giveHi, 3000);
+console.log(giveHiSometimes()); // -> 'hi'
+ setTimeout(function() { console.log(giveHiSometimes()); }, 2000); // -> undefined
+ setTimeout(function() { console.log(giveHiSometimes()); }, 4000); // -> undefined
+setTimeout(function() { console.log(giveHiSometimes()); }, 8000); // -> 'hi'
