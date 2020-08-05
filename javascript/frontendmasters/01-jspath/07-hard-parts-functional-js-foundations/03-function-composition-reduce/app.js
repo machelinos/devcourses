4db306f94a6ecb4c629971b1e7c2e74ee5824c24
Marcel Aristides Cabrera Gomez <marcel.cabrera@gmail.com>
2020-08-05 16:30:34 -0500

    FEM - The Hard parts : Functional JS foundations - Function composition with reduce

---
+++ javascript/frontendmasters/01-jspath/07-hard-parts-functional-js-foundations/03-function-composition-reduce/app.js
@@ -0,0 +1,17 @@
+const multipliBy2 = x => x*2;
+const add3 = x => x+3;
+const divideBy5 = x => x/5;
+
+const reduce = (array, howToCombine, buildingUp)=> {
+    for(let i=0; i<array.length; i++){
+        buildingUp = howToCombine(buildingUp, array[i]);
+    }
+    return buildingUp;
+}
+
+const runFunctionOnInput = (input, fn) => {
+    return fn(input);
+}
+
+const output = reduce([multipliBy2, add3, divideBy5], runFunctionOnInput, 11);
+const outputNative = [multipliBy2, add3, divideBy5].reduce(runFunctionOnInput, 11);
