e416cda7b7c608444ba3b718a9e598b642bc120f
machelinos <marcel.cabrera@gmail.com>
2021-01-02 02:32:49 -0600

    FEM - JS from fundamentals to functional - 06 _.filter()

---
+++ fem/beginner-path/01-js-fundamentals-to-functional/06-filter/app.js
@@ -0,0 +1,100 @@
+//Implement _.filter
+let _ = {};
+
+_.filter = function(list, fn) {
+    if(typeof(list)==='object'){
+        let output = [];
+        if(Array.isArray(list)){
+            for(let i=0; i<list.length; i++){
+                if(fn(list[i], i, list)) output.push(list[i]);
+            }
+
+        }else{
+            for(let key in list){
+                if(fn(list[key], key, list)) output.push(list[key]);
+            }
+        }
+        return output;
+    } else {
+        alert("please use a list as input");
+    }
+}
+
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+let filteredSuspects = _.filter(videoData, function(value, index, list){
+    return value.present;
+});
