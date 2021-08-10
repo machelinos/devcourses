6b4a4d37a2eb954df104e38bc611671859b683c6
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-08-10 09:28:37 -0500

    L02 - Dev tools configurations

--- javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
+++ javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
@@ -1,22 +1,22 @@
-const Pet = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement('h2', {}, props.name),
-        React.createElement('h3', {}, props.animal),
-        React.createElement('h3', {}, props.breed),
-    ]);
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h1', {}, 'Adopt Me!'),
-            React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese' }),
-            React.createElement(Pet, { name: 'Gatis', animal: 'Cat', breed: 'Street' }),
-            React.createElement(Pet, { name: 'Nemo', animal: 'Cat', breed: 'Movie teather cat' }),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, { name: "Gatis", animal: "Cat", breed: "Street" }),
+    React.createElement(Pet, {
+      name: "Nemo",
+      animal: "Cat",
+      breed: "Movie teather cat",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
