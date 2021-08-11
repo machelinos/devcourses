bb17c76674ab36b9253907c5fa6b907ae3df7a23
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-08-11 07:17:58 -0500

    L03 Core react - 01 Using JSX

--- javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
+++ javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
-
+/* 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
@@ -17,6 +17,18 @@ const App = () => {
       breed: "Movie teather cat",
     }),
   ]);
-};
+}; */
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese"></Pet>
+      <Pet name="Gatis" animal="Cat" breed="Street"></Pet>
+      <Pet name="Nemo" animal="Cat" breed="Movie teather cat"></Pet>
+
+    </div>
+  )
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
