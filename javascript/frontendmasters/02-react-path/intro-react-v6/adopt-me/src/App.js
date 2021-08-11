f878dda5d131620231474fc75a4ced3e67a13770
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-08-11 08:44:50 -0500

    L03 Core react - 06 07 Adding more hooks

--- javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
+++ javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchParams from './SearchParams';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 /* 
 const App = () => {
   return React.createElement("div", {}, [
@@ -24,9 +24,8 @@ const App = () => {
     <div>
       <h1>Adopt Me!</h1>
       <SearchParams></SearchParams>
-
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/SearchParams.js
+++ javascript/frontendmasters/02-react-path/intro-react-v6/adopt-me/src/SearchParams.js
@@ -1,18 +1,65 @@
 import { useState } from "react";
 
+const ANIMALS = ["Bird", "Cat", "Dog", "Pig"];
+const BREEDS = [];
+
 const SearchParams = () => {
-    const [location, setLocation] = useState("Monterrey, N.L.");
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    Location
-                    <input id="location" value={location} onChange={e=>setLocation(e.target.value)} placeholder="Location" type="text" />
-                </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
-}
+  const [location, setLocation] = useState("Monterrey, N.L.");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+            type="text"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={e=>setAnimal(e.target.value)}
+          >
+              <option />
+              {
+                  ANIMALS.map(animal=>{
+                      return(
+                          <option value={animal} key={animal}>{animal}</option>
+                      )
+                  })
+              }
+          </select>
+        </label>
+        <label htmlFor="animal">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={e=>setBreed(e.target.value)}
+          >
+              <option />
+              {
+                  BREEDS.map(breed=>{
+                      return(
+                          <option value={breed} key={breed}>{breed}</option>
+                      )
+                  })
+              }
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
