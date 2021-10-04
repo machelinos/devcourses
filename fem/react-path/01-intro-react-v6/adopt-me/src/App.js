5e2b911209c3677f124ca8da1ee1b8de6ef9e9a4
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-04 09:58:58 -0500

    FEM - React path - L02 React JS tools - prettier

--- fem/react-path/01-intro-react-v6/adopt-me/src/App.js
+++ fem/react-path/01-intro-react-v6/adopt-me/src/App.js
@@ -1,26 +1,22 @@
 const Pet = (props) => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h2', {}, props.name),
-            React.createElement('h3', {}, props.animal),
-            React.createElement('h4', {}, props.breed),
-        ]
-    );
-}
-
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-            React.createElement(Pet, { name: 'Gatis', animal: 'Cat', breed: 'Street' }),
-            React.createElement(Pet, { name: 'Garrapo', animal: 'Dog', breed: 'Street' }),
-            React.createElement(Pet, { name: 'Perris', animal: 'Dog', breed: 'Husky' }),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h4", {}, props.breed),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, { name: "Gatis", animal: "Cat", breed: "Street" }),
+    React.createElement(Pet, {
+      name: "Garrapo",
+      animal: "Dog",
+      breed: "Street",
+    }),
+    React.createElement(Pet, { name: "Perris", animal: "Dog", breed: "Husky" }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
