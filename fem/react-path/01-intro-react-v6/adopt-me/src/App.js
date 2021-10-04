153fc3ac344ba11af6413713b3793bb319fccab2
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-04 07:37:29 -0500

    FEM - React path - L01 No frills react - Component props

--- fem/react-path/01-intro-react-v6/adopt-me/src/App.js
+++ fem/react-path/01-intro-react-v6/adopt-me/src/App.js
@@ -1,11 +1,11 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement(
         'div',
         {},
         [
-            React.createElement('h2', {}, 'Gatis'),
-            React.createElement('h3', {}, 'Cat'),
-            React.createElement('h4', {}, 'Street'),
+            React.createElement('h2', {}, props.name),
+            React.createElement('h3', {}, props.animal),
+            React.createElement('h4', {}, props.breed),
         ]
     );
 }
@@ -13,10 +13,12 @@ const Pet = () => {
 const App = () => {
     return React.createElement(
         "div",
-        { id: 'my-brand'},
+        {},
         [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
+            React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+            React.createElement(Pet, { name: 'Gatis', animal: 'Cat', breed: 'Street' }),
+            React.createElement(Pet, { name: 'Garrapo', animal: 'Dog', breed: 'Street' }),
+            React.createElement(Pet, { name: 'Perris', animal: 'Dog', breed: 'Husky' }),
         ]
     );
 };
