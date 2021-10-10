6397ff29f58fbdfedc12ca1a8765cc6c40a2a14a
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-10 12:07:55 -0500

    FEM - JS path - JS fundaments to funcional - L01 Objects - Destructuring

---
+++ fem/js-path/01-js-fundamentals-functional/ex-02-destructuring/index.js
@@ -0,0 +1,10 @@
+// Create an object thatm looks like this
+// {name: 'Rusty', room: 'kitchen', weapon: 'candlestick'}
+const obj = {
+    name: 'Rusty',
+    room: 'kitchen',
+    weapon: 'candlestick'
+}
+//Extract out the weapon and location using destructuring
+
+const { weapon, room } = obj;
