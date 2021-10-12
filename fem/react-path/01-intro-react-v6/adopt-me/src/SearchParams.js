1782d21b03f34504c63958679bcd5757f394f230
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-12 08:31:05 -0500

    FEM - React path - L03 Core concepts- Hooks intro - adding more hooks (animals and breeds)

--- fem/react-path/01-intro-react-v6/adopt-me/src/SearchParams.js
+++ fem/react-path/01-intro-react-v6/adopt-me/src/SearchParams.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   // without destructuring
   // const locationTuple = useState("Seattle, WA");
   // const location = locationTuple[0];
   // const setLocation = locationTuple[1];
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -19,6 +25,46 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return (
+                <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => {
+              return (
+                <option value={breed} key={breed}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
