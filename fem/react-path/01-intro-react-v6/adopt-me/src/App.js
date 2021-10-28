1f1414f781495e874e716094a8603205b3b13578
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-28 00:02:40 -0500

    FEM - React path - L04 React Capabilities - React Router - Route and Link

--- fem/react-path/01-intro-react-v6/adopt-me/src/App.js
+++ fem/react-path/01-intro-react-v6/adopt-me/src/App.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 /* const App = () => {
   return React.createElement("div", {}, [
@@ -18,10 +20,30 @@ import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me!</h1>
-      <SearchParams></SearchParams>
+      <Router>
+        <header>
+          <Link to="/">
+            <h1 id="my-brand">Adopt Me!</h1>
+          </Link>
+        </header>
+
+        <Switch>
+          <Route path="/details/:id">
+            <Details></Details>
+          </Route>
+
+          <Route path="/">
+            <SearchParams></SearchParams>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
---
+++ fem/react-path/01-intro-react-v6/adopt-me/src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>HI whatup!</h2>;
+};
+
+export default Details;
