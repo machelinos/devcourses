c9de0b64bfbaf46bce2515d4389e0993d90234f1
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-10-31 23:29:24 -0600

    FEM - React path - L04 React Capabilities - Class components

--- fem/react-path/01-intro-react-v6/adopt-me/src/Details.js
+++ fem/react-path/01-intro-react-v6/adopt-me/src/Details.js
@@ -1,9 +1,40 @@
-import { useParams } from "react-router";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = { loading: true };
+    }
 
-const Details = () => {
-    const { id } = useParams();
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
+    }
 
-    return <h2>HI whatup! {id}</h2>;
-};
+    render() {
+        console.log(this.state);
 
-export default Details;
+        if (this.state.loading) {
+            return <h2>loading...</h2>
+        }
+
+        const { animal, breed, city, state, description, name } = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(Details);
