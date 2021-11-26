0108a5bbcf8c8ec0f6eb1bad76207cbf4dab85c3
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-11-25 23:12:20 -0600

    Platzi - intro to react - Todo app - components initial structure

---
+++ platzi/react/01-intro-to-do/src/TodoSearch.js
@@ -0,0 +1,7 @@
+function TodoSearch() {
+    return (
+        <input placeholder="Cebolla" />
+    )
+}
+
+export { TodoSearch };
---
+++ platzi/react/01-intro-to-do/src/Todoitem.js
@@ -0,0 +1,11 @@
+const TodoItem = (props) => {
+    return (
+        <li>
+            <span>C</span>
+            <p>{ props.text }</p>
+            <span>X</span>
+        </li>
+    )
+}
+
+export { TodoItem };
