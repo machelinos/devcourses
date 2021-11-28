86ff4a5a036271f97265b3a5e4b6c6f986ff71a0
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-11-28 13:37:06 -0600

    Platzi - intro to react - Todo app - local ui user events

--- platzi/react/01-intro-to-do/src/CreateTodoButton.js
+++ platzi/react/01-intro-to-do/src/CreateTodoButton.js
@@ -1,8 +1,18 @@
 import "./CreateTodoButton.css";
 
 const CreateTodoButton = () => {
+
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton"
+            onClick={()=>{onClickButton('Aqui va a abrir el modal')}}
+        >
+            +
+        </button>
     )
 }
 
--- platzi/react/01-intro-to-do/src/Todoitem.js
+++ platzi/react/01-intro-to-do/src/Todoitem.js
@@ -1,11 +1,34 @@
 import "./TodoItem.css";
 
 const TodoItem = (props) => {
+    const onComplete = () => {
+        alert(`Ya completaste el todo: ${props.text}`);
+    }
+
+    const onDelete = () => {
+        alert(`Ya borraste el todo ${props.text}`);
+    }
+
     return (
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed ? 'Icon-check--active' : ''}`}>√</span>
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{ props.text }</p>
-            <span className="Icon Icon-delete">X</span>
+            <span
+                className={`Icon Icon-check ${props.completed ? 'Icon-check--active' : ''}`}
+                onClick={onComplete}
+            >
+                √
+            </span>
+            <p
+                className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}
+            >
+                { props.text }
+            </p>
+
+            <span
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
+                X
+            </span>
         </li>
     )
 }
