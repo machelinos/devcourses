3edbf07c8a33c2e1789d27236bcdc45d9c68f2d5
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-11-28 22:37:35 -0600

    Platzi - intro to react - Todo app - state - counting and searching todos

--- platzi/react/01-intro-to-do/src/App.js
+++ platzi/react/01-intro-to-do/src/App.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./Todoitem";
 import { CreateTodoButton } from "./CreateTodoButton";
-//import './App.css';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Intro a react', completed: true },
+  { text: 'Whatever', completed: false },
+];
 
 function App() {
-  const todos = [
-    { text: 'Cortar cebolla', completed: false },
-    { text: 'Intro a react', completed: true },
-    { text: 'Whatever', completed: false },
-  ];
+  const [todos, setTodos] = useState(defaultTodos);
+  const [searchValue, setSearchValue] = useState('');
+
+  const completedTodos = todos.filter( todo=>!!todo.completed ).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if( searchValue.length > 0 ) {
+    searchedTodos = todos.filter( todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()) );
+  } else {
+    searchedTodos = todos;
+  }
+
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total = {totalTodos}
+        completed = {completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
--- platzi/react/01-intro-to-do/src/TodoSearch.js
+++ platzi/react/01-intro-to-do/src/TodoSearch.js
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-    const [searchValue, setSearchValue] = useState('');
-
+function TodoSearch({ searchValue, setSearchValue }) {
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
