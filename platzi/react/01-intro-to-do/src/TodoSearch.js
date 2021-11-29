1eb5700387bc76a2c614bde29b5e9901a8037582
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-11-28 21:29:30 -0600

    Platzi - intro to react - Todo app - useState in TodoSearch

--- platzi/react/01-intro-to-do/src/TodoSearch.js
+++ platzi/react/01-intro-to-do/src/TodoSearch.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
-    return (
+    return [
         <input
             className="TodoSearch"
+            key="input"
             placeholder="Cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    )
+        />,
+        <p key="paragraph">{ searchValue }</p>
+    ]
 }
 
 export { TodoSearch };
