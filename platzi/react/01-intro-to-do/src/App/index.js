5c044f06e53bcb401388323dec07022a2743c022
Marcel Cabrera <marcel.cabrera@gmail.com>
2021-12-01 07:57:31 -0600

    Platzi - intro to react - Todo app - Custom hooks for localStorage

--- platzi/react/01-intro-to-do/src/App/index.js
+++ platzi/react/01-intro-to-do/src/App/index.js
@@ -7,16 +7,30 @@ import { AppUI } from "./AppUI";
   { text: 'Whatever', completed: false },
 ];
  */
-function App() {
-  let localStorageTodos = localStorage.getItem('TODOS_V1');
 
-  if(!localStorageTodos) {
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+function useLocalStorage(item, initialValue) {
+  let localStorageItems = localStorage.getItem(item);
+
+  if(!localStorageItems) {
+    localStorage.setItem(item,JSON.stringify(initialValue));
   }
 
-  let parsedTodos = JSON.parse(localStorageTodos);
+  let parsedItems = JSON.parse(localStorageItems);
 
-  const [todos, setTodos] = useState(parsedTodos);
+  const [items, setItems] = useState(parsedItems);
+
+  const saveItems = (items) => {
+    const stringifiedItems = JSON.stringify(items);
+    localStorage.setItem(item, stringifiedItems);
+    setItems(items);
+  }
+
+  return [items, saveItems];
+
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = useState('');
 
   const completedTodos = todos.filter( todo=>!!todo.completed ).length;
@@ -28,12 +42,6 @@ function App() {
     searchedTodos = todos.filter( todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()) );
   } else {
     searchedTodos = todos;
-  }
-
-  const saveTodos = (todos) => {
-    const stringifiedTodos = JSON.stringify(todos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(todos);
   }
 
   const completeTodo = (text) => {
